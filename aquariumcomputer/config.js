2c4647a9bdb1481180866af3cbb55434d9b43d63
alien2fg <filip.gnojek@gmail.com>
2024-09-05 12:50:52 +0200

    d

---
+++ aquariumcomputer/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWCgA5Cw8jKgz6z_F9xNGQiU372IOFTo4",
+    authDomain: "aqua-computer.firebaseapp.com",
+    databaseURL: "https://aqua-computer-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "aqua-computer",
+    storageBucket: "aqua-computer.appspot.com",
+    messagingSenderId: "269598410424",
+    appId: "1:269598410424:web:529e7d98dbc9350777d64d"
+};
+
+export default config;
--- aquariumcomputer/firebase.js
+++ aquariumcomputer/firebase.js
@@ -1,18 +1,10 @@
 import firebase from "firebase/compat/app";
-import "firebase/database";
+import "firebase/compat/database";
+import config from './config.js'; // Import config
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCWCgA5Cw8jKgz6z_F9xNGQiU372IOFTo4",
-    authDomain: "aqua-computer.firebaseapp.com",
-    databaseURL: "https://aqua-computer-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "aqua-computer",
-    storageBucket: "aqua-computer.appspot.com",
-    messagingSenderId: "269598410424",
-    appId: "1:269598410424:web:529e7d98dbc9350777d64d"
-  };
-
-if (!firebase.app.length) {
-    firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
 }
 
 const db = firebase.database();
